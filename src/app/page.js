942531009d5a678792e81228c2dc7b6d421a7403
Shahed Ahmad <shaheda.a16@gmail.com>
2023-05-18 14:50:24 +0430

    fetch results

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,14 @@
-export default function Home() {
+import Results from "./components/Results"
+
+const API_KEY = process.env.API_KEY
+export default async function Home({searchParams}) {
+  const genre = searchParams.genre || "fetchTrending"
+const res = await fetch(`https://api.themoviedb.org/3/${genre === "fetchTopRated" ? "movie/top_rated": "trending/all/week"}?api_key=${API_KEY}`, {next: {revalidate: 10000}} )
+const data = await res.json()
+const results = data.results
   return (
-    <h1>Project</h1>
+    <div>
+      <Results results={results} />
+    </div>
   )
 }
